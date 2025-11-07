3a580feb7efeff2f5794875775603c2f0adfd848
Thanaporn-Jampathip <tanaporn7249.kk@gmail.com>
2025-11-07 15:17:52 +0700

    fixed CSS

--- login_staff.php
+++ login_staff.php
@@ -15,10 +15,9 @@
     <script src="https://cdn.jsdelivr.net/npm/sweetalert2@11"></script>
     <style>
         @import url('https://fonts.googleapis.com/css2?family=IBM+Plex+Sans+Thai+Looped:wght@100;200;300;400;500;600;700&family=IBM+Plex+Sans+Thai:wght@100;200;300;400;500;600;700&family=Prompt:ital,wght@0,100;0,200;0,300;0,400;0,500;0,600;0,700;0,800;0,900;1,100;1,200;1,300;1,400;1,500;1,600;1,700;1,800;1,900&display=swap');
-        body {
+        body, html {
             font-family: "Prompt", sans-serif;
             background: linear-gradient(to bottom right, #0457f1ff, #A8EFFF);
-            max-height: 100%;
             height: 100vh;
             color: white;
         }
